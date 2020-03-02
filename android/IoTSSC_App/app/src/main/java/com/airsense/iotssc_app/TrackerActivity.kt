1a5d9526218f810130f6a0ba99f71eb788f972e6
rphi <8237136+rphi@users.noreply.github.com>
2020-03-02 12:41:21 +0000

    Add firestore logging

--- android/IoTSSC_App/app/src/main/java/com/airsense/iotssc_app/TrackerActivity.kt
+++ android/IoTSSC_App/app/src/main/java/com/airsense/iotssc_app/TrackerActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.bluetooth.BluetoothAdapter
+import android.location.Location
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -16,7 +17,12 @@ import com.harrysoft.androidbluetoothserial.BluetoothManager
 import com.harrysoft.androidbluetoothserial.SimpleBluetoothDeviceInterface
 
 import com.airsense.iotssc_app.utils.DataLogger
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.gson.Gson
 import java.util.concurrent.TimeUnit
+import com.airsense.iotssc_app.TrackerActivity.SerialMessage as SerialMessage1
 
 class TrackerActivity : AppCompatActivity() {
 
@@ -28,6 +34,10 @@ class TrackerActivity : AppCompatActivity() {
     private lateinit var deviceInterface: SimpleBluetoothDeviceInterface
     private lateinit var bluetoothReceiver: BluetoothReceiver
     private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var functions: FirebaseFunctions
+
+    private lateinit var firestore: FirebaseFirestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         app = applicationContext as ApplicationData
@@ -54,16 +64,93 @@ class TrackerActivity : AppCompatActivity() {
                 .build()
         WorkManager.getInstance(applicationContext).enqueue(dataLoggerWorkRequest)
 
-
-
+        functions = FirebaseFunctions.getInstance()
+        firestore = FirebaseFirestore.getInstance()
     }
 
+    data class SerialMessage(
+            val status: String = "none",
+            val dust: Float? = null,
+            val nh3: Float? = null,
+            val co: Float? = null,
+            val no2: Float? = null,
+            val c3h8: Float? = null,
+            val c4h10: Float? = null,
+            val ch4: Float? = null,
+            val h2: Float? = null,
+            val c2h5oh: Float? = null,
+            val voc: Float? = null,
+            val eco2: Float? = null
+    )
+
     fun onMessageRecieved(message: String){
-        runOnUiThread{
-            val text = findViewById<EditText>(R.id.editText2)
-            text.setText(message)
-            Toast.makeText(this, "Updated", Toast.LENGTH_LONG)
+
+        val response = Gson().fromJson(message, SerialMessage::class.java);
+
+        if (response.status == "reading") {
+            runOnUiThread {
+                Toast.makeText(this, "updating...", Toast.LENGTH_LONG).show()
+            }
         }
+
+        if (response.status == "values") {
+            runOnUiThread {
+                val text = findViewById<EditText>(R.id.editText2)
+                text.setText(message)
+                Toast.makeText(this, "updated", Toast.LENGTH_LONG).show()
+            }
+
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+
+                if (location != null) {
+                    val doc = hashMapOf(
+                            "dust" to response.dust,
+                            "nh3" to response.nh3,
+                            "co" to response.co,
+                            "no2" to response.no2,
+                            "c3h8" to response.c3h8,
+                            "ch4h10" to response.c4h10,
+                            "ch4" to response.ch4,
+                            "h2" to response.h2,
+                            "c2h5oh" to response.c2h5oh,
+                            "voc" to response.voc,
+                            "eco2" to response.eco2,
+                            "lat" to location.latitude,
+                            "long" to location.longitude
+                    )
+
+                    firestore.collection("readings").add(doc)
+
+                } else {
+                    Log.i("tac", "got null location")
+                }
+            }
+
+
+        }
+
+        /*
+        On send character
+        {
+            "status":"reading"
+        }
+
+        Example sensor data JSON
+        {
+            "dust":199.40,
+            "nh3":6.88,
+            "co":0.58,
+            "no2":0.12,
+            "c3h8":20239.26,
+            "c4h10":8246.12,
+            "ch4":0.36,
+            "h2":0.03,
+            "c2h5Oh":0.11,
+            "voc":0,
+            "eco2":400
+         }
+         */
     }
 
     fun requestUpdate() {
